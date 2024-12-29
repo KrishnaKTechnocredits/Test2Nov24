e824580736402208380e4f2723d77b802f814a8f
Krishna Kanani <kananikrishna427@gmail.com>
2024-12-29 11:33:49 +0530

    TOCheck rebase

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -4,6 +4,7 @@ public class PracClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello_class1, branch A CHnages for second scenario commit 1 change");
-		System.out.println("lAst commit ");
+		System.out.println("lAst commit with rebase chanages ");
+		System.out.println("lAst commit with rebase chanages before push on master");
 	}
 }
