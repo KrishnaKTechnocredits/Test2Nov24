8568ec388d75451aea28c63cd7eccc7124ca6b7c
Krishna Kanani <kananikrishna427@gmail.com>
2024-12-29 12:07:18 +0530

    Revert "Reverted Commit_2"
    This reverts commit 001fa781f41df836dd58c85cad9df77ffec2c8c1.

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -5,6 +5,5 @@ public class PracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1, branch A CHnages for second scenario");
 		System.out.println("Old brnach commit");
-		System.out.println("Commit 2");
 	}
 }
