a37a0f411043e12a2e859d34ce34bf4d2def50fa
Krishna Kanani <kananikrishna427@gmail.com>
2024-12-29 08:17:29 +0530

    Commit_1

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -3,6 +3,6 @@ package practice;
 public class PracClass1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello_class1, branch A CHnages for second scenario");
+		System.out.println("Hello_class1, branch A CHnages for second scenario commit 1 change");
 	}
 }
